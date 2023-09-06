ad9bcf3d2ac8e6277f8946fef20ffb23708453d5
Paulina Dąbrowska <paulina.dabrovska@gmail.com>
2023-09-06 19:02:11 +0200

    animated hamburger menu

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ const navbar = document.querySelector('.js-middle-section');
 const cart = document.querySelector('.js-cart');
 
 menu.onclick = () =>{
+  menu.classList.toggle('close-menu');
   navbar.classList.toggle('open');
   cart.classList.toggle('non-visible');
 }
