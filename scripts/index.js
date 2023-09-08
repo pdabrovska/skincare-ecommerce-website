baad0c54e6be43d372ff51a42b71138a0deac51d
Paulina Dąbrowska <paulina.dabrovska@gmail.com>
2023-09-08 14:46:11 +0200

    add to slider to index.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,14 @@
 import { navbar } from "./utils/general.js";
 
 navbar();
+
+//slider
+
+var counter = 1;
+setInterval(()=>{
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4){
+    counter = 1;
+  }
+}, 5000);
