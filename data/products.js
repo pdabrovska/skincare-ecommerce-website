2456388b5949321e2f4b626a672984dfcca81e90
Paulina Dąbrowska <paulina.dabrovska@gmail.com>
2023-09-12 23:07:50 +0200

    added dragable carousel to bestsellers in index

---
+++ data/products.js
@@ -0,0 +1,8 @@
+export const products = [
+  {
+    name: 'x',
+    id: 'xxx01',
+    section: 'face',
+    price: 1023,
+  }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -12,3 +12,39 @@ setInterval(()=>{
     counter = 1;
   }
 }, 15000);
+
+//product carousel
+
+const carousel = document.querySelector('.js-products-container');
+//firstImg = carousel.querySelectorAll('.product-card')[0];
+const arrowIcons = document.querySelectorAll('.js-arrow');
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+//let firstImgWidth = firstImg.clientWidth + 25;
+/*
+arrowIcons.forEach( icon =>{
+  icon.addEventListener('click', ()=>{
+    carousel.scrollLeft
+  });
+});
+*/
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX;
+  prevScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if(!isDragStart) return;
+  e.preventDefault();
+  let positionDiff = e.pageX - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+};
+
+const dragStop = () => {
+  isDragStart = false;
+}
+
+carousel.addEventListener('mousedown', dragStart);
+carousel.addEventListener('mousemove', dragging);
+carousel.addEventListener('mouseup', dragStop);
