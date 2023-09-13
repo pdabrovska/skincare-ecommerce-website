208fded5d4624d5abc0b08ff74b503cd1203e381
Paulina Dąbrowska <paulina.dabrovska@gmail.com>
2023-09-13 23:57:20 +0200

    filters in shop.html responsive

--- scripts/shop.js
+++ scripts/shop.js
@@ -4,3 +4,7 @@ import { products } from "../data/products.js";
 navbar();
 
 generateProducts(products, 'every');
+
+document.querySelector('.js-body-products').addEventListener('click', ()=>{
+  console.log(1);
+});
