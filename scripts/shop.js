785a201f119788622aa06a2a2d0554633dbb4bf8
Paulina Dąbrowska <paulina.dabrovska@gmail.com>
2023-09-15 14:17:59 +0200

    added price range slider to shop.html

--- scripts/shop.js
+++ scripts/shop.js
@@ -21,3 +21,56 @@ resetBtn.addEventListener('click', ()=>{
   generateProducts(products, 'every');
   document.querySelector('input[name="filter-button"][id="all"]').checked = true;
 });
+
+//price range
+
+window.onload = function(){
+  slideMin();
+  slideMax();
+};
+
+const minVal = document.querySelector('.min-val');
+const maxVal = document.querySelector('.max-val');
+
+const minTooltip = document.querySelector('.min-tooltip');
+const maxTooltip = document.querySelector('.max-tooltip');
+
+const minGap = 0;
+const range = document.querySelector('.slider-track');
+const sliderMinValue = parseInt(minVal.min);
+const sliderMaxValue = parseInt(maxVal.max);
+
+function slideMin(){
+  let gap = parseInt(maxVal.value) - parseInt(minVal.value);
+  if(gap <= minGap){
+    minVal.value = parseInt(maxVal.value) - minGap;
+  }
+
+  minTooltip.innerHTML = `$${minVal.value}`;
+  setArea()
+};
+
+function slideMax(){
+  let gap = parseInt(maxVal.value) - parseInt(minVal.value);
+  if(gap <= minGap){
+    maxVal.value = parseInt(minVal.value) + minGap;
+  }
+
+  maxTooltip.innerHTML = `$${maxVal.value}`;
+  setArea()
+};
+
+document.querySelector('.min-val').addEventListener('input', ()=>{
+  slideMin();
+});
+
+document.querySelector('.max-val').addEventListener('input', ()=>{
+  slideMax();
+});
+
+function setArea(){
+  range.style.left = (minVal.value / sliderMaxValue) * 100 + '%';
+  minTooltip.style.left =(minVal.value / sliderMaxValue) * 100 + '%';
+  range.style.right = 100- (maxVal.value / sliderMaxValue) * 100 + '%';
+  maxTooltip.style.right = 100- (maxVal.value / sliderMaxValue) * 100 + '%';
+}
