b92002615a51e1b7354a3134d3a92887a50dfa82
Paulina Dąbrowska <paulina.dabrovska@gmail.com>
2023-09-20 00:28:07 +0200

    added changes to collection.html

--- scripts/collections.js
+++ scripts/collections.js
@@ -1,5 +1,7 @@
-import { navbar, updateCartAmount } from "./utils/general.js";
+import { navbar, updateCartAmount, generateProducts } from "./utils/general.js";
 import { cart } from "../data/cart.js";
+import { products } from "../data/products.js";
 
 navbar();
 updateCartAmount(cart);
+generateProducts(products, 'body', 0, 120000, cart)
